4a0a9602295c3639f0d4e8af58cc5ff842846264
Raz Levi <66571396+razlevio@users.noreply.github.com>
2021-09-11 14:00:04 +0300

    Create marioRegular.c

---
+++ pset1/mario/marioRegular.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <ctype.h>
+
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            if (i + j < height - 1)
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("#");
+            }
+        }
+        printf("\n");
+    }
+}
