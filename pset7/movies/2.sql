c555e1bc140e42079c214a5e56fa9c3ff530d1fd
levio <66571396+levio7@users.noreply.github.com>
2021-09-23 13:02:43 +0300

    solution

---
+++ pset7/movies/2.sql
@@ -0,0 +1,4 @@
+/* write a SQL query to determine the birth year of Emma Stone.
+   Your query should output a table with a single column and a single row (not including the header) containing Emma Stone’s birth year. */
+
+SELECT birth FROM people WHERE name = 'Emma Stone';
