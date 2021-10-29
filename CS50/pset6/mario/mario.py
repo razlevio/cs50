e3cf312c01a8206064a58cd5eb2cf02829879567
Raz Levi <66571396+razlevio@users.noreply.github.com>
2021-10-29 20:18:22 +0300

    Update mario.py

--- CS50/pset6/mario/mario.py
+++ CS50/pset6/mario/mario.py
@@ -1,16 +1,22 @@
 from cs50 import get_int
 
-height = 0
-while height > 8 or height < 1:
-    height = get_int("Height: ")
+def main():
+    height = 0
+    while height > 8 or height < 1:
+        height = get_int("Height: ")
 
-for i in range(1, height + 1):
-    for j in range(1, height + 1):
-        if j > (height - i):
-            print("#", end="")
-        else:
-            print(" ", end="")
-        # Print middle space and remaining hash
-        if j == height:
-            print(" ", "#" * i, end="")
-    print()
+    # Loop through rows and then columns
+    for i in range(1, height + 1):
+        for j in range(1, height + 1):
+            # Start printing # after correct number of spaces
+            if j > (height - i):
+                print("#", end="")
+            else:
+                print(" ", end="")
+            # Print middle space and remaining #
+            if j == height:
+                print(" ", "#" * i, end="")
+        print()
+
+if __name__ == "__main__":
+    main()
