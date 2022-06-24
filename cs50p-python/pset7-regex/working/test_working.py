028800825a78e21f81d38c5efaccd0ea43b24d1c
Raz Levi <66571396+razlevio@users.noreply.github.com>
2022-06-24 14:45:07 +0300

    Create test_working.py

---
+++ cs50p-python/pset7-regex/working/test_working.py
@@ -0,0 +1,16 @@
+from working import convert
+import pytest
+
+def test_valid_conversions():
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+    assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
+    assert convert("10 PM to 8 AM") == "22:00 to 08:00"
+    assert convert("10:30 PM to 8:50 AM") == "22:30 to 08:50"
+
+def test_wrong_format():
+    with pytest.raises(ValueError):
+        convert("9 AM - 9 PM")
+
+def test_wrong_hour():
+    with pytest.raises(ValueError):
+        convert("9:60 AM to 13:76 PM")
